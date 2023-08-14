54570e43f4881c691f217a9fec0d906800e4964a
Nnse <107156665+Nnse@users.noreply.github.com>
2023-08-14 14:57:42 +0900

    更新

--- assets/js/template/meta.js
+++ assets/js/template/meta.js
@@ -1,12 +1,13 @@
 function insertMeta(home_path = '', description = '') {
     document.write(...[
-        '<link rel="shortcut icon" type="image/x-icon" href="' + home_path + 'favicon.ico">',
-        '<link rel="icon" href="https://avatars.githubusercontent.com/u/107301051?s=200&v=4">',
-        '<link rel="apple-touch-icon" href="https://avatars.githubusercontent.com/u/107301051?s=200&v=4">',
-        '<meta name="twitter:card" content="summary" />',
-        '<meta name="twitter:site" content="@BowyersMC" />',
-        '<meta name="twitter:title" content="BowyersMC" />',
-        '<meta name="twitter:description" content="' + description + '" />',
-        '<meta name="twitter:image" content="https://pbs.twimg.com/profile_images/1560502728945967104/WL_X3SAN_400x400.jpg" />'
+        '    <link rel="shortcut icon" type="image/x-icon" href="' + home_path + 'favicon.ico">' + "\n",
+        '    <link rel="icon" href="https://avatars.githubusercontent.com/u/107301051?s=200&v=4">' + "\n",
+        '    <link rel="apple-touch-icon" href="https://avatars.githubusercontent.com/u/107301051?s=200&v=4">' + "\n",
+        '    <meta name="apple-mobile-web-app-capable" content="yes" />' + "\n",
+        '    <meta name="twitter:card" content="summary" />' + "\n",
+        '    <meta name="twitter:site" content="@BowyersMC" />' + "\n",
+        '    <meta name="twitter:title" content="BowyersMC" />' + "\n",
+        '    <meta name="twitter:description" content="' + description + '" />' + "\n",
+        '    <meta name="twitter:image" content="https://pbs.twimg.com/profile_images/1560502728945967104/WL_X3SAN_400x400.jpg" /> + "\n'
     ]);
 }
