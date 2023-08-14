539160bb5c9b94d48d45f9d2f759eebc508839de
Nnse <107156665+Nnse@users.noreply.github.com>
2023-08-14 12:11:48 +0900

    Revert "テンプレート"
    This reverts commit 87ff3e936a1519ab3a0a246be69c32ddc4399a95.

--- assets/js/template/header.js
+++
@@ -1,8 +0,0 @@
-function insertHeader(home_path = '') {
-    let header = document.getElementById('header');
-    header.innerHTML =
-        '                  <a href="' + home_path + 'index.html" class="logo">BowyersMC Website</a>' + "\n" +
-        '                  <ul class="icons">  ' + "\n" +
-        '                    <li><a href="https://discord.gg/bVRvSeKfTF" class="icon brands fa-discord"><span class="label">Discord</span></a></li>' + "\n" +
-        '                  </ul>';
-}
--- assets/js/template/meta.js
+++
@@ -1,9 +0,0 @@
-function insertMeta(description = '') {
-    document.write(...[
-        '<meta name="twitter:card" content="summary" />',
-        '<meta name="twitter:site" content="@BowyersMC" />',
-        '<meta name="twitter:title" content="BowyersMC" />',
-        '<meta name="twitter:description" content="' + description + '" />',
-        '<meta name="twitter:image" content="https://pbs.twimg.com/profile_images/1560502728945967104/WL_X3SAN_400x400.jpg" />'
-    ]);
-}
